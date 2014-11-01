43f68c36ffa5c31333d5010e6ea8fb65d30b7a34
Vamsi Krishna Sridhara <vamsisridhara@gmail.com>
2014-11-01 13:12:51 -0400

    restful data

---
+++ RestfulTutorial.Data/BlogPost.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulTutorial.Data
+{
+
+    [DataContract]
+    public class BlogPost
+    {
+        [DataMember(Name = "id")]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Name = "title")]
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        [Column("Url")]
+        [DataMember(Name = "url")]
+        public string UriString
+        {
+            get
+            {
+                return Url == null ? null : Url.ToString();
+            }
+            set
+            {
+                Url = value == null ? null : new Uri(value);
+            }
+        }
+
+        [NotMapped]
+        public Uri Url
+        {
+            get;
+            set;
+        }
+    }
+}
