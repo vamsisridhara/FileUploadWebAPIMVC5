6ce98dcb070b3622526752c4d1757ae923f1dbe1
Vamsi Krishna Sridhara <vamsisridhara@gmail.com>
2016-10-17 19:14:06 -0400

    tets

---
+++ FileUploadWebAPIMVC5/Controllers/DBValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FileUploadWebAPIMVC5.Controllers
+{
+    public class dbtest
+    {
+        public int id { get; set; }
+        public bool flag { get; set; }
+    }
+    [RoutePrefix("api/db")]
+    public class DBValuesController : ApiController
+    {
+        [HttpGet]
+        [Route("filltable")]
+        public IHttpActionResult getdata()
+        {
+            return Ok(new List<dbtest>()
+            {
+                new dbtest() { id =1 , flag = true },
+                new dbtest() { id =2 , flag = false }
+
+            });
+        }
+    }
+}
